291309334f0e8e8da09506898093d192858354c7
tasslakti <tasnimlakti2@gmail.com>
2025-11-30 15:30:31 +0100

    ajout des entités certification et review

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -17,7 +21,7 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $nomComplet = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
@@ -27,70 +31,63 @@ class User
     private ?string $telephone = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $role = null;
+    private ?string $role = 'ROLE_USER';
 
-    public function getId(): ?int
+    #[ORM\OneToMany(targetEntity: Reservation::class, mappedBy: 'user')]
+    private Collection $reservations;
+
+    #[ORM\OneToMany(targetEntity: Review::class, mappedBy: 'user')]
+    private Collection $reviews;
+
+    #[ORM\OneToMany(targetEntity: Notification::class, mappedBy: 'user')]
+    private Collection $notifications;
+
+    public function __construct()
     {
-        return $this->id;
+        $this->reservations = new ArrayCollection();
+        $this->reviews = new ArrayCollection();
+        $this->notifications = new ArrayCollection();
     }
 
-    public function getNomComplet(): ?string
+    // Getters et Setters existants
+    public function getId(): ?int { return $this->id; }
+    public function getNomComplet(): ?string { return $this->nomComplet; }
+    public function setNomComplet(string $nomComplet): static { $this->nomComplet = $nomComplet; return $this; }
+    public function getEmail(): ?string { return $this->email; }
+    public function setEmail(string $email): static { $this->email = $email; return $this; }
+    public function getTelephone(): ?string { return $this->telephone; }
+    public function setTelephone(?string $telephone): static { $this->telephone = $telephone; return $this; }
+
+    // Méthodes pour l'authentification
+    public function getMotDePasse(): ?string { return $this->motDePasse; }
+    public function setMotDePasse(string $motDePasse): static { $this->motDePasse = $motDePasse; return $this; }
+
+    public function getRole(): ?string { return $this->role; }
+    public function setRole(string $role): static { $this->role = $role; return $this; }
+
+    // Implémentation des interfaces de sécurité
+    public function getRoles(): array
     {
-        return $this->nomComplet;
+        return [$this->role];
     }
 
-    public function setNomComplet(string $nomComplet): static
-    {
-        $this->nomComplet = $nomComplet;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getMotDePasse(): ?string
+    public function getPassword(): string
     {
         return $this->motDePasse;
     }
 
-    public function setMotDePasse(string $motDePasse): static
+    public function getUserIdentifier(): string
     {
-        $this->motDePasse = $motDePasse;
-
-        return $this;
+        return $this->email;
     }
 
-    public function getTelephone(): ?string
+    public function eraseCredentials(): void
     {
-        return $this->telephone;
+        // Si tu stockes des données temporaires sensibles, efface-les ici
     }
 
-    public function setTelephone(?string $telephone): static
-    {
-        $this->telephone = $telephone;
-
-        return $this;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(string $role): static
-    {
-        $this->role = $role;
-
-        return $this;
-    }
+    // Relations
+    public function getReservations(): Collection { return $this->reservations; }
+    public function getReviews(): Collection { return $this->reviews; }
+    public function getNotifications(): Collection { return $this->notifications; }
 }
